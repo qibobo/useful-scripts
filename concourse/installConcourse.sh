a27acfa758d25bedc224c83a7b32fa0f1720054c
qiyangbj@cn.ibm.com <qiyangbj@cn.ibm.com>
2017-09-12 15:40:34 +0800

    concourse

--- concourse/installConcourse.sh
+++ concourse/installConcourse.sh
@@ -12,24 +12,24 @@ POSTGRES_PORT="$9"
  
 CONCOURSE_VERSION=${CONCOURSE_VERSION:-v3.4.0}
  
-# apt-get update
-# apt-get install -y postgresql postgresql-contrib
+apt-get update
+apt-get install -y postgresql postgresql-contrib
  
-# sudo -u postgres createuser -s concourse
-# sudo -u postgres createdb --owner=concourse concourse
+sudo -u postgres createuser -s concourse
+sudo -u postgres createdb --owner=concourse concourse
  
-# curl -L -f -o /usr/local/bin/concourse https://github.com/concourse/concourse/releases/download/$CONCOURSE_VERSION/concourse_linux_amd64
-# chmod +x /usr/local/bin/concourse
+curl -L -f -o /usr/local/bin/concourse https://github.com/concourse/concourse/releases/download/$CONCOURSE_VERSION/concourse_linux_amd64
+chmod +x /usr/local/bin/concourse
  
-# mkdir -p /etc/concourse
-# ssh-keygen -t rsa -f /etc/concourse/host_key -N ''
-# ssh-keygen -t rsa -f /etc/concourse/session_signing_key -N ''
-# ssh-keygen -t rsa -f /etc/concourse/worker_key -N ''
-# cp /etc/concourse/worker_key.pub /etc/concourse/authorized_worker_keys
+mkdir -p /etc/concourse
+ssh-keygen -t rsa -f /etc/concourse/host_key -N ''
+ssh-keygen -t rsa -f /etc/concourse/session_signing_key -N ''
+ssh-keygen -t rsa -f /etc/concourse/worker_key -N ''
+cp /etc/concourse/worker_key.pub /etc/concourse/authorized_worker_keys
  
-# adduser --system --group concourse
-# chgrp concourse /etc/concourse/*
-# chmod g+r /etc/concourse/*
+adduser --system --group concourse
+chgrp concourse /etc/concourse/*
+chmod g+r /etc/concourse/*
  
 cat >/etc/systemd/system/concourse_web.service <<-EOF
 [Unit]
@@ -38,14 +38,14 @@ After=postgres.service
 
 [Service]
 ExecStart=/usr/local/bin/concourse web  \
-        --basic-auth-username $ADMIN_USERNAME  \
-        --basic-auth-password $ADMIN_PASSWORD \
-        --session-signing-key /etc/concourse/session_signing_key  \
-        --tsa-host-key /etc/concourse/host_key  \
-        --tsa-authorized-keys /etc/concourse/authorized_worker_keys  \
-        --external-url http://$EXTERNAL_URL_HOST_NAME:$EXTERNAL_PORT  \
-        --postgres-data-source postgres://$POSTGRES_USER:$POSTGRES_PWD@localhost:5432/concourse \
-        --tsa-bind-port $TSA_PORT
+--basic-auth-username $ADMIN_USERNAME  \
+--basic-auth-password $ADMIN_PASSWORD \
+--session-signing-key /etc/concourse/session_signing_key  \
+--tsa-host-key /etc/concourse/host_key  \
+--tsa-authorized-keys /etc/concourse/authorized_worker_keys  \
+--external-url http://$EXTERNAL_URL_HOST_NAME:$EXTERNAL_PORT  \
+--postgres-data-source postgres://$POSTGRES_USER:$POSTGRES_PWD@localhost:5432/concourse \
+--tsa-bind-port $TSA_PORT
 
 User=concourse
 Group=concourse
@@ -63,11 +63,11 @@ After=concourse_web.service
 
 [Service]
 ExecStart=/usr/local/bin/concourse worker \
-       --work-dir /var/lib/concourse \
-       --tsa-host localhost \
-       --tsa-public-key /etc/concourse/host_key.pub \
-       --tsa-worker-private-key /etc/concourse/worker_key \
-       --tsa-port $TSA_PORT
+--work-dir /var/lib/concourse \
+--tsa-host localhost \
+--tsa-public-key /etc/concourse/host_key.pub \
+--tsa-worker-private-key /etc/concourse/worker_key \
+--tsa-port $TSA_PORT
 
 User=root
 Group=root
